8d91ecec47c835f668f2763ef2748af2832a5629
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2015-12-06 16:49:49 +0100

    remove unused import

--- src/com/pixel/exceptions/PaymentException.java
+++ src/com/pixel/exceptions/PaymentException.java
@@ -2,6 +2,11 @@ package com.pixel.exceptions;
 
 public class PaymentException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5882316938430468789L;
+
 	public PaymentException(String message) {
 		super(message);
 	}
